c209c97719adf7c22dda4ec71eaa5b0afec9e567
Unknown <bogdyich@gmail.com>
2018-03-18 19:30:46 +0300

    Added class SubaruWRX

---
+++ Homework #02/src/SubaruWRX.java
@@ -0,0 +1,10 @@
+public class SubaruWRX extends Car {
+
+    public SubaruWRX() {
+        this.model = "Subaru WRX";
+        this.manufacturerCountry = "Japan";
+        this.maxSpeed = 215;
+        this.weight = 1465;
+        this.productionYear = 2014;
+    }
+}
